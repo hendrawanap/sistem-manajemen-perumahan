466af4213c8495b33212d09d6851a16a539275e6
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2021-11-07 10:55:24 +0700

    Add Jadwal Index & Controller (2)

---
+++ coreui/src/views/pages/Jadwal/dummy.js
@@ -0,0 +1,325 @@
+const kebersihan = {
+    senin: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    selasa: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    rabu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    kamis: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    jumat: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    sabtu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    minggu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+}
+
+const keamanan = {
+    senin: {
+        shift_1: [
+            'Arif Wiranata',
+            'Arif Wiranata',
+            'Arif Wiranata',
+        ],
+        shift_2: [
+            'Arif Wiranata',
+        ],
+        shift_3: [
+            'Arif Wiranata',
+            'Arif Wiranata',
+            'Arif Wiranata',
+            'Arif Wiranata',
+            'Arif Wiranata',
+        ],
+    },
+    selasa: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    rabu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    kamis: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    jumat: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    sabtu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    minggu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+}
+
+const keserasian = {
+    senin: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    selasa: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    rabu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    kamis: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    jumat: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    sabtu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    minggu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+}
+
+const kontrol = {
+    senin: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    selasa: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    rabu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    kamis: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    jumat: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    sabtu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    minggu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+}
+
+const administrasi = {
+    senin: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    selasa: {
+        shift_1: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_2: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+        shift_3: [
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+            'Hendrawan Anantha Putra',
+        ],
+    },
+    rabu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    kamis: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    jumat: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    sabtu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+    minggu: {
+        shift_1: [],
+        shift_2: [],
+        shift_3: [],
+    },
+}
+
+export default {
+    kebersihan,
+    keamanan,
+    keserasian,
+    kontrol,
+    administrasi,
+}
