e0ab371a21a592bb4f210e41bce0af5a7383c5b7
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2021-11-12 22:52:06 +0700

    Laporan Integrations

--- coreui/src/views/pages/Laporan/LaporanTagihan.vue
+++ coreui/src/views/pages/Laporan/LaporanTagihan.vue
@@ -9,97 +9,132 @@
         </CCardHeader>
         <CCardBody>
           <CForm v-on:submit.prevent>
-            <CInput label="Nama Tagihan" disabled type="text" v-model="namaTagihan" />
-            <CInput label="Tanggal" disabled type="date" v-model="tanggal"/>
+            <CInput
+              label="Nama Tagihan"
+              disabled
+              type="text"
+              v-model="tagihan.namaTagihan"
+            />
+            <CInput
+              label="Tanggal"
+              disabled
+              type="date"
+              v-model="tagihan.tanggal"
+            />
             Rincian
-              <CCard>
-                <CRow class = "ml-2 mr-2" v-for = "t in tagihan" :key = " t.tipe ">
+            <CCard>
+              <CCardBody>
+                <CRow v-for="t in tagihan.rincian" :key="t.tipe">
                   <CCol>
-                    <CInput label="Tipe" disabled type="text" v-model="t.tipe"/>
+                    <CInput
+                      label="Tipe"
+                      disabled
+                      type="text"
+                      v-model="t.tipe"
+                    />
                   </CCol>
                   <CCol>
-                    <CInput label="Nominal" disabled type="number" v-model="t.nominal"/>
+                    <CInput
+                      label="Nominal"
+                      disabled
+                      type="number"
+                      v-model="t.nominal"
+                    />
                   </CCol>
                 </CRow>
-              </CCard>
+              </CCardBody>
+            </CCard>
             <CInput label="Total Tagihan" disabled v-model="total" />
           </CForm>
         </CCardBody>
       </CCard>
 
       <CCard>
-      <CCardHeader>
-        <CIcon name="cil-justify-center"/><strong>Statistik Tagihan</strong>
-      </CCardHeader>
-      <CCardBody>
-        <CCard>
-            <CRow class="ml-2 mr-2 d-flex justify-content-between">
-                <CCol class = "mt-4" >
-                    <CInput  label="Jumlah KK" disabled type="number" v-model="jumlahKK"/>
-                </CCol>
-                <CCol>
-                    <CInput label="Total Lunas" disabled type="number" v-model="sudahLunas"/>
-                    <CInput label="Belum Lunas" disabled type="number" v-model="belumLunas"/>
-                </CCol>
-            </CRow>
-        </CCard>
-    </CCardBody>
-    </CCard>
+        <CCardHeader>
+          <CIcon name="cil-justify-center" /><strong>Statistik Tagihan</strong>
+        </CCardHeader>
+        <CCardBody>
+          <CCardGroup>
+            <CWidgetProgressIcon
+              :header="sudahLunas"
+              :value="(sudahLunas / jumlahKK) * 100"
+              text="Sudah Lunas"
+              color="success"
+              inverse
+            >
+              <CIcon name="cil-notes" height="36" />
+            </CWidgetProgressIcon>
+            <CWidgetProgressIcon
+              :header="belumLunas"
+              :value="(belumLunas / jumlahKK) * 100"
+              text="Belum Lunas"
+              color="danger"
+              inverse
+            >
+              <CIcon name="cil-notes" height="36" />
+            </CWidgetProgressIcon>
+          </CCardGroup>
+        </CCardBody>
+      </CCard>
     </CCol>
   </CRow>
-  
 </template>
 
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "LaporanTagihan",
-  title: 'Laporan Tagihan',
-  computed : {
-      total() {
-          return this.tagihan.map(t => t.nominal).reduce((total,nominal) => total + nominal)
-      },
-      belumLunas() {
-          return this.jumlahKK-this.sudahLunas
-      },
+  title: "Laporan Tagihan",
+  computed: {
+    total() {
+      return this.tagihan.rincian
+        .map((t) => t.nominal)
+        .reduce((total, nominal) => total + nominal);
+    },
+    belumLunas() {
+      return this.jumlahKK - this.sudahLunas;
+    },
   },
   data() {
     return {
-        tagihan: [
-            {
-                tipe: "Iuran Keamanan",
-                nominal: 400000,
-            },
-            {
-                tipe: "Iuran Kebersihan",
-                nominal: 200000,
-            },
-            {
-                tipe: "Iuran Kontrol",
-                nominal: 100000,
-            }
-        ],
-        namaTagihan: "Tagihan Bulan Oktober",
-        tanggal: "2021-10-20",
-        jumlahKK: 0,
-        sudahLunas: 20,
-        items : [],
-        fields: ['no','id'],
-        
-        
-
+      tagihan: {
+        namaTagihan: "",
+        tanggal: "",
+        rincian: [],
+      },
+      jumlahKK: 0,
+      sudahLunas: 0,
     };
   },
-  methods :{
-      getTotalKK() {
-      axios.get(this.$apiAdress + '/api/KK').then( r => {
-        this.jumlahKK = r.data.length
-      });
+  methods: {
+    fetchLaporanTagihan() {
+      const bulan = this.$route.query.bulan;
+      const tahun = this.$route.query.tahun;
+      axios
+        .get(
+          this.$apiAdress +
+            "/api/laporanTagihan?bulan=" +
+            bulan +
+            "&tahun=" +
+            tahun +
+            "&token=" +
+            localStorage.getItem("api_token")
+        )
+        .then((r) => {
+          const tagihan = r.data.tagihan;
+          this.tagihan = {
+            namaTagihan: tagihan.namaTagihan,
+            tanggal: tagihan.tanggalTagihan,
+            rincian: JSON.parse(tagihan.rincian),
+          };
+          this.jumlahKK = r.data.jumlahKK;
+          this.sudahLunas = r.data.sudah;
+        });
     },
   },
   mounted() {
-      this.getTotalKK();
-  }
+    this.fetchLaporanTagihan();
+  },
 };
 </script>
---
+++ laravel/app/Http/Controllers/LaporanController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KK;
+use App\Models\Pembayaran;
+use App\Models\Presensi;
+use App\Models\Tagihan;
+use Illuminate\Http\Request;
+
+class LaporanController extends Controller
+{
+    public function getLaporanPresensi(Request $request) {
+        $tahun = $request->query('tahun');
+        $bulan = $request->query('bulan');
+        if ($bulan < 10) {
+            $bulan = '0' + (int) $bulan;
+        }
+        $tanggalAwal = "$tahun-$bulan-01";
+        $akhirBulan = date('t', strtotime($tanggalAwal));
+        $tanggalAwal = $tanggalAwal . ' 00:00:00';
+        $tanggalAkhir = "$tahun-$bulan-$akhirBulan 23:59:59";
+        $presensi = Presensi::where('waktuPresensi', '>=', $tanggalAwal)
+                            ->where('waktuPresensi', '<=', $tanggalAkhir)
+                            ->join('employees', 'employees.id', '=', 'presences.idPegawai')
+                            ->select('presences.*', 'employees.nama')
+                            ->orderBy('waktuPresensi')
+                            ->get();
+        $presensi1 = $presensi->where('waktuPresensi', '>=', "$tahun-$bulan-01" . " 00:00:00")
+                            ->where('waktuPresensi', '<=', "$tahun-$bulan-01" . " 23:59:59")
+                            ->whereIn('status',['Tepat Waktu', 'Terlambat'])->toArray();
+        $output = array();
+        
+        $presensiController = new PresensiController();
+        
+        for ($i=1; $i <= $akhirBulan; $i++) {
+            $tanggal = $i;
+            if ($i < 10){
+                $tanggal = '0' . $i;
+            }
+            $semua = $presensi->where('waktuPresensi', '>=', "$tahun-$bulan-$tanggal" . " 00:00:00")
+                        ->where('waktuPresensi', '<=', "$tahun-$bulan-$tanggal" . " 23:59:59");
+            $hadir = $semua->whereIn('status',['Tepat Waktu', 'Terlambat'])->count();
+            $alpha = count($presensiController->getPresensiAlpha("$tahun-$bulan-$tanggal"));
+            $semua = $semua->count() + $alpha;
+
+            array_push($output, [
+                'tanggal' => "$tahun-$bulan-$tanggal",
+                'presensi' => $hadir . '/' . $semua
+            ]);
+        }
+        return response()->json($output);
+        // return response()->json(['tahun' => $tahun, 'bulan' => $bulan]);
+    }
+
+    public function getLaporanTagihan(Request $request) {
+        $tahun = $request->query('tahun');
+        $bulan = $request->query('bulan');
+        $tanggalAwal = "$tahun-$bulan-01";
+        $akhirBulan = date('t', strtotime($tanggalAwal));
+        $tanggalAkhir = "$tahun-$bulan-$akhirBulan";
+        $jumlahKK = KK::all()->count();
+        $tagihan = Tagihan::whereBetween('tanggalTagihan', [$tanggalAwal, $tanggalAkhir])
+                            ->take(1)
+                            ->get()
+                            ->toArray();
+        if ($tagihan) {
+            $sudah = Pembayaran::where('idTagihan',$tagihan[0]['id'])
+                        ->join('families', 'families.id', '=', 'payments.idKK')
+                        ->select('payments.id', 'payments.tanggalBayar', 'families.nomorKK', 'families.kepalaKeluarga')
+                        ->get()
+                        ->count();
+            // $belum = 
+            return response()->json(['jumlahKK' => $jumlahKK, 'sudah' => $sudah, 'belum' => $jumlahKK - $sudah, 'tagihan' => $tagihan[0]]);
+        } else {
+            return response()->json(['message' => 'Tidak Ada Tagihan']);
+        }
+    }
+}
